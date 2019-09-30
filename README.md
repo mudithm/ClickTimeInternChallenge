7fead7e20a6efe221184f4df700ae5a17f446072
mudithm <mudithm@gmail.com>
2019-09-30 13:09:22 -0700

    Updated README, separated styles and scripts

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-__Response to Question 1__
+__Response to Question 1, _The Race Around the World___
 
 This is a simple jquery-based stopwatch that incorporates both the stretch goals specified for this question.
 
-You can run it as follow:
+You can run it as follows:
 
 1. Download/clone this repository
-2. From within the folder, start a python server with `python -m http.server <port>`
+2. From within the folder containing , start a python server with `python -m http.server <port>`
 3. Navigate to the url `localhost:<port>`
 
+Assumptions: I assumed that, whenever the start button is clicked, the start time (from which elapsed time is calculated) gets reset. Additionally, I assumed that clicking the stop button acts like the "lap" function on a stopwatch.
--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+// Entry in the HistoryTable
 class HistoryTableEntry {
+    // constructor. Elaped time defaults to 0
+    // sets date(UTC), latitude, longitude
     constructor(date, latitude, longitude, elapsed_time = '00:00:00') {
         this.date = date;
         this.latitude = latitude;
@@ -7,12 +10,13 @@ class HistoryTableEntry {
         this.elapsed_time = elapsed_time;
     }
 
-
+    // returns a table row in html to be displayed in the history table
     get entry() {
+        console.log(this.date);
         const time = two_d_date(this.date.getHours()) + ":" + two_d_date(this.date.getMinutes()) + ":" + two_d_date(this.date.getSeconds());
         const date = two_d_date(this.date.getMonth() + 1) + "-" + two_d_date(this.date.getDate()) + "-" + this.date.getFullYear();
         const entry = "<tr>" +
-            "<td>" + date + " " + time + "</td>" +
+            "<td>" + date + " " + time + " (" + this.date.toLocaleDateString(navigator.language, { timeZoneName: 'short' }).split(',')[1] + " ) " + "</td>" +
             "<td>" + this.latitude + "</td>" +
             "<td>" + this.longitude + "</td>" +
             "<td>" + this.elapsed_time + "</td>" +
@@ -21,10 +25,12 @@ class HistoryTableEntry {
         return entry;
     }
 
+    // return the date, in milliseconds (UTC time)
     getDateTime() {
         return this.date.getTime();
     }
 
+    // return the time elapsed since the last start
     elapsedTime() {
         var ms = this.universal_date - getCurrentStart();
         const seconds = two_d_date(parseInt((ms / 1000) % 60));
@@ -39,172 +45,68 @@ class HistoryTableEntry {
         return elapsed;
     }
 
+    // set the elapsed time; necessary to reset elapsed_time field
+    // for "stop" entries
     set_elapsed_time() {
         this.elapsed_time = this.elapsedTime();
     }
 
 }
 
+// HistoryTable; holds HistoryTableEntry objets in an array
 class HistoryTable {
+    // constructor; takes array of HistoryTableEntry objects
     constructor(entries) {
         this.entries = entries;
     }
 
+    // adds HistoryTableEntry to the array
     addEntry(entry) {
         console.log("entry: ");
         console.log(entry);
         this.entries.push(entry);
     }
 
+    // clear HistoryTable array
     clear() {
         this.entries.length = 0;
     }
 
+    // return length of entries
     get length() {
         return this.entries.length;
     }
 
+    // return entries array
     getEntries() {
         return this.entries;
     }
 
 }
 
-// default 
+// default globals
 var entries = [];
 window.history_table = new HistoryTable(entries);
 window.history_size = 0;
 
+// default storage functions
 var setCurrentStart = function(value) {};
 var getCurrentStart = function() { return 0; };
 var addCurrentHistory = function(value) {};
 var getCurrentHistory = function() { return 0; };
 var clear = function() {};
 
-// Code begins here
-if (storageAvailable('localStorage')) {
-    console.log("local storage available");
-
-    setCurrentStart = function(value) {
-        localStorage.setItem('current_start', value);
-        window.current_start = value;
-    };
-
-    getCurrentStart = function() {
-        return localStorage.getItem('current_start');
-    };
-
-    getCurrentHistory = function() {
-        const len = localStorage.getItem('history_size');
-        var out = [];
-        if (len === null)
-            return new HistoryTable(out);
-
-        for (let i = 0; i < len; i++) {
-            var item = JSON.parse(localStorage.getItem('history_table' + i));
-            out[i] = new HistoryTableEntry(new Date(item["date"]), item["latitude"], item["longitude"], item["elapsed_time"]);
-        }
-
-        console.log("len");
-        console.log(len);
-
-        return new HistoryTable(out);
-    };
-
-
-    if (getCurrentStart() == null)
-        setCurrentStart(0);
-    var entries = [];
-    const hist = getCurrentHistory();
-    if (hist.length != 0) {
-        // update table...
-        window.history_table = hist;
-        window.history_size = localStorage.getItem('history_size');
-        entry_list = hist.getEntries();
-        for (let i = 0; i < window.history_size; i++) {
-            $("#history tbody").append(
-                entry_list[i].entry
-            ).fadeIn(10);
-            if (entry_list[i].getDateTime() == getCurrentStart()) {
-                console.log("last start");
-
-                $('tr').last().addClass('last_start');
-            }
-
-        }
-
-        $('tr').last().filter("[class!='last_start']").addClass('last_stop');
-        $('.last_start').nextUntil("last_stop", "[class!='last_stop']").addClass('current_block');
-
-
-        scroll_table();
-
-        console.log(getCurrentHistory());
-        console.log(window.history_table);
-    } else {
-        console.log("empty");
-    }
-
-    addCurrentHistory = function(value) {
-        console.log("hi");
-        console.log(JSON.stringify(value));
-        const index = 'history_table' + window.history_table.length;
-        window.history_size++;
-
-        localStorage.setItem(index, JSON.stringify(value));
-        localStorage.setItem('history_size', window.history_size);
-
-
-        window.history_table.addEntry(value);
-
-        console.log(window.history_table);
-    };
-
-    clear = function() {
-        localStorage.clear();
-        window.history_table = new HistoryTable([]);
-        window.current_start = 0;
-        window.history_size = 0;
-        $('tbody').hide().empty().fadeIn();
-    }
-} else {
-    window.current_start = 0;
-    var entries = []
-    window.history_table = new HistoryTable(entries);
-
-    setCurrentStart = function(value) {
-        window.current_start = value;
-    }
-
-    getCurrentStart = function() {
-        return window.current_start;
-    }
-
-    addCurrentHistory = function(value) {
-        window.history_table.addEntry(value);
-    }
-
-    getCurrentHistory = function() {
-        return window.history_table;
-    }
-
-    setCurrentStart(0);
-    var entries = [];
-    setCurrentHistory(new HistoryTable(entries))
-
-    clear = function() {
-        window.history_table = new HistoryTable([]);
-        window.current_start = 0;
-        window.history_size = 0;
-    }
-
-}
-
+// function to run if geolocation successful 
 function loc_success(pos, date) {
+    // initialize new htable entry
     const htable_ent = new HistoryTableEntry(date,
         pos.coords.latitude, pos.coords.longitude);
+
+    // set elapsed time
     htable_ent.set_elapsed_time();
     addCurrentHistory(htable_ent);
+
+    // add entry to table and update styles
     $("#history tbody").append(
         htable_ent.entry
     ).fadeIn(200);
@@ -220,26 +122,33 @@ function loc_success(pos, date) {
         $('.last_start').nextUntil("last_stop", "[class!='last_stop']").addClass('current_block');
     }
 
+    //scroll to bottom of table
     scroll_table();
 
 
 }
 
+// if geolocation unsuccessful
 function loc_error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
     return null;
 }
 
+// geolocation options
 var loc_options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
 };
 
+
+// start button onclick handler
 $('#start').on("click", function() {
     const date = new Date();
 
+    // set current start time
     setCurrentStart(date.getTime());
+
     // Check whether geolocation is available
     // on the current device
     if ("geolocation" in navigator) {
@@ -255,7 +164,9 @@ $('#start').on("click", function() {
 
 });
 
+// stop button onclick handler
 $('#stop').on("click", function() {
+    // if current start not set, notify user
     if (getCurrentStart() == 0) {
         console.log("not yet started");
         $("#history tbody").html(
@@ -281,17 +192,19 @@ $('#stop').on("click", function() {
 
 });
 
+// reset button onclick handler
 $('#reset').on("click", function() {
 
     clear();
 
 });
 
-
+// convert date values to two digits, if necessary
 function two_d_date(value) {
     return value < 10 ? '0' + value : '' + value;
 }
 
+// scroll to bottom of table
 function scroll_table() {
     var table_height = $('tbody').prop('scrollHeight');
     var scrolled_height = $("tbody").innerHeight() + $("tbody").scrollTop();
@@ -329,4 +242,149 @@ function storageAvailable(type) {
             // acknowledge QuotaExceededError only if there's something already stored
             (storage && storage.length !== 0);
     }
+}
+
+
+// main code
+
+// if local storage is available, set storage functions
+// else, use default storage
+if (storageAvailable('localStorage')) {
+    console.log("local storage available");
+
+    // set current start function
+    // store current start in localStorage 
+    setCurrentStart = function(value) {
+        localStorage.setItem('current_start', value);
+        window.current_start = value;
+    };
+
+    // get current start function
+    // return current start from local storage
+    getCurrentStart = function() {
+        return localStorage.getItem('current_start');
+    };
+
+    // get current history table
+    // return current history table, or empty if none exists
+    getCurrentHistory = function() {
+        const len = localStorage.getItem('history_size');
+        var out = [];
+        if (len === null)
+            return new HistoryTable(out);
+
+        // populate entries from table
+        for (let i = 0; i < len; i++) {
+            var item = JSON.parse(localStorage.getItem('history_table' + i));
+            out[i] = new HistoryTableEntry(new Date(item["date"]), item["latitude"], item["longitude"], item["elapsed_time"]);
+        }
+
+        console.log("len");
+        console.log(len);
+
+        return new HistoryTable(out);
+    };
+
+    // initialize globals
+    if (getCurrentStart() == null)
+        setCurrentStart(0);
+    var entries = [];
+    const hist = getCurrentHistory();
+    // if table in storage is not empty, populate globals & table
+    if (hist != null && hist.length != 0) {
+        // update table...
+        window.history_table = hist;
+        window.history_size = localStorage.getItem('history_size');
+        entry_list = hist.getEntries();
+        for (let i = 0; i < window.history_size; i++) {
+            $("#history tbody").append(
+                entry_list[i].entry
+            ).fadeIn(10);
+            // mark last start position
+            if (entry_list[i].getDateTime() == getCurrentStart()) {
+                console.log("last start");
+
+                $('tr').last().addClass('last_start');
+            }
+
+        }
+
+        // update styles
+        $('tr').last().filter("[class!='last_start']").addClass('last_stop');
+        $('.last_start').nextUntil("last_stop", "[class!='last_stop']").addClass('current_block');
+
+        // scroll to bottom of table
+        scroll_table();
+
+        console.log(getCurrentHistory());
+        console.log(window.history_table);
+    } else {
+        console.log("empty");
+    }
+
+    // add to current history 
+    // if local_storage, add entry to local storage
+    addCurrentHistory = function(value) {
+        console.log("hi");
+        console.log(JSON.stringify(value));
+        const index = 'history_table' + window.history_table.length;
+        window.history_size++;
+
+        localStorage.setItem(index, JSON.stringify(value));
+        localStorage.setItem('history_size', window.history_size);
+
+
+        window.history_table.addEntry(value);
+
+        console.log(window.history_table);
+    };
+
+    // clear function
+    // if local storage, clear
+    clear = function() {
+        localStorage.clear();
+        window.history_table.clear();
+        window.current_start = 0;
+        window.history_size = 0;
+        $('tbody').hide(800).empty().fadeIn();
+    }
+} else {
+    // set globals
+    window.current_start = 0;
+    var entries = []
+    window.history_table = new HistoryTable(entries);
+
+    // set current start from global
+    setCurrentStart = function(value) {
+        window.current_start = value;
+    }
+
+    // get current start from global
+    getCurrentStart = function() {
+        return window.current_start;
+    }
+
+    // add current history from global
+    addCurrentHistory = function(value) {
+        window.history_table.addEntry(value);
+    }
+
+    // get current history from global
+    getCurrentHistory = function() {
+        return window.history_table;
+    }
+
+    // create empty historytable
+    setCurrentStart(0);
+    var entries = [];
+    setCurrentHistory(new HistoryTable(entries))
+
+    // clear globals and empty table
+    clear = function() {
+        window.history_table = new HistoryTable([]);
+        window.current_start = 0;
+        window.history_size = 0;
+        $('tbody').hide().empty().fadeIn();
+    }
+
 }
